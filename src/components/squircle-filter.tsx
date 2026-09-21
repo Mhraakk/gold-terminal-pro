4a7f4f2c8cd91d91be3ed9b92339c367dda570a3
Mhraakk <155156453+Mhraakk@users.noreply.github.com>
2026-09-21 17:22:04 +0000

    زرین: گوئی بین دکمهٔ صدا و تم

--- src/components/squircle-filter.tsx
+++ src/components/squircle-filter.tsx
@@ -12,6 +12,16 @@ export function SquircleFilter() {
           />
           <feBlend in="SourceGraphic" in2="goo" />
         </filter>
+        <filter id="za-gooey">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="4.4" result="blur" />
+          <feColorMatrix
+            in="blur"
+            mode="matrix"
+            values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 20 -7"
+            result="goo"
+          />
+          <feBlend in="SourceGraphic" in2="goo" />
+        </filter>
       </defs>
     </svg>
   );
